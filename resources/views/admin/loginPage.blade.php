50f196a91496027d973b70965fecb5b9f0445143
poppotaufikgalli <poppo.galli@gmail.com>
2023-08-30 09:10:36 +0700

    add logo

--- resources/views/admin/loginPage.blade.php
+++ resources/views/admin/loginPage.blade.php
@@ -14,15 +14,14 @@
 	</style>
 	<div class="container-fluid" style="height: 100vh">
 		<div class="row">
-			<div class="col-4">
+			<div class="col-md-4 col-12">
 				<div class="d-flex justify-content-center align-items-center" style="height : 100%">
 					<div class="card border-0 card-body">
 						<div class="p-5">
 							<div class="text-center">
 								<a href="/">
-									<img src="{{asset('images/SIMPERS2.PNG')}}" alt="app_logo" class="img-fluid">
+									<img src="{{asset('images/simpers2.png')}}" alt="app_logo" class="img-fluid">
 								</a>
-								
 							</div>
 							<form class="user" method="post" action="{{route('login.perform')}}">
 								@csrf
@@ -38,7 +37,14 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-8" id="imgC"></div>			
+			<div class="col-md-8 col position-relative" id="imgC">
+				<div class="position-absolute top-0 start-0">
+					<img class="ms-3 mt-3" src="{{asset('images/mako.png')}}" alt="app_logo" class="img-fluid" width="350">
+				</div>
+				<div class="position-absolute bottom-0 end-0">
+					<img class="me-5" src="{{asset('assets/img/Logo_berakhlak_bangga.webp')}}" alt="app_logo" class="img-fluid" width="350">
+				</div>
+			</div>			
 		</div>
 		
 	</div>
