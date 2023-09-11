09258eee227ccb403c6cc5fde4645b612814d125
poppotaufikgalli <poppo.galli@gmail.com>
2023-09-11 07:37:59 +0700

    add https

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <title>@yield('title')</title>
     <!-- Custom fonts for this template-->
     <!--<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
